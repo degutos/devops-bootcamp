0de41f3675bba97d2a1df4ae91eacd64f33b97eb
degutos <degutos@null.computer>
2024-06-10 07:11:56 +0100

    added decision.py with IF statement

---
+++ python/decisions.py
@@ -0,0 +1,65 @@
+# Making Decisions
+
+# Comparison operators
+
+# ==    !=      >       >=      <       <=
+
+
+# Lets use operator == Equal
+
+print(2 == 2)
+
+print(2 == 4)
+
+print("Hello!" == "Hello!")
+
+print(4 == (2 * 2))
+
+
+# Lets use operator != Not Equal
+
+print(2 != 2)
+
+print(2 != 4)
+
+print("Hello!" != "Hello!")
+
+print(4 != (2 * 2))
+
+
+
+# Lets use operator > grater than
+
+print(4 > 2)
+
+print(2 > 4)
+
+print(2 > 2)
+
+
+cost_of_apple = 2
+cost_of_banana = 3
+
+print(cost_of_apple > cost_of_banana)
+
+
+# IF statement
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are an adult! ")
+
+
+if age >= 18:
+    if age == 18:
+        print("You are exactly 18 years old! ")
+    else:
+        print("You are older than 18 years old")
+
+
+
+
+
+
+
