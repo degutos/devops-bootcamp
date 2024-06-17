058eba5557573a06389222e3fc89d433ba10c5fb
degutos <degutos@null.computer>
2024-06-17 07:13:05 +0100

    updated lists.py with methods like append,insert,pop,len,sort

--- python/lists.py
+++ python/lists.py
@@ -24,3 +24,78 @@ print(list1[::-1])
 # Print first and item [3] in the list
 list1 = [10, 11, 12, 13, 14]
 print(list1[::3])
+
+
+
+# Lists - Methods
+
+
+countries = ["USA","Canada","India"]
+countries.append("Spain")
+countries.insert(2,"Italy")
+print(countries)
+
+
+# swapping values in the list - logical way
+
+temp = countries[0]
+countries[0] = countries[1]
+countries[1] = temp
+print(countries)
+
+
+# swapping values back in the list - easy way
+countries[0], countries[1] = countries[1], countries[0]
+print(countries)
+
+
+
+# Sort
+countries.sort()
+print(countries)
+
+# Reverse
+countries.reverse()
+print(countries)
+
+
+# Another sort examples:
+ages = [56, 72, 24, 46]
+ages.sort()
+print(ages)
+
+num = [4, 4, 3, 1]
+num.sort()
+print(num)
+
+
+# Another example of append
+list1 = [10, 20, 30, 40, 50]
+list1.append(60)
+print(list1)
+
+# function max and min
+list1=["Go","Java","C","Python"]
+print(max(list1))
+
+list1=["Go","Java","C","Rust"]
+print(min(list1))
+
+# Another example of Insert method, this will print:  UK 8 India Canada
+list1=['UK','India','Canada']
+
+list1.insert(1,8)
+
+print(list1)
+
+
+# how to remove one item of the list
+list1 = [4, 4, 3, 1]
+list1.pop(2)
+print(list1)
+
+
+
+# using len function 
+list1=['h', 'e', 'l', 'l', 'o']
+print(len(list1))
