74a48fe6e333a091e4ecc1783350a36ca75373a9
degutos <degutos@null.computer>
2024-06-17 06:47:55 +0100

    added lists.py

---
+++ python/lists.py
@@ -0,0 +1,26 @@
+# Lists
+
+# normal list
+list1 = [1, 2, 3, 4, 5]
+print(list1[4])
+
+# Changing a value in the list
+numbers = [1, 2, 3, 4, 5]
+numbers[4] = 6
+print(numbers[4])
+
+list1 = [1, 2, 3, 4, 5]
+list1[0] = 10
+print(list1)
+
+
+
+
+# Showing Reverse List
+list1=[2,5,3,1]
+print(list1[::-1])
+
+
+# Print first and item [3] in the list
+list1 = [10, 11, 12, 13, 14]
+print(list1[::3])
